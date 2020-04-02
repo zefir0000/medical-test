07aabf7ac07221c9fc68e0d128cc1996b9e1217b
slawek <>
2020-04-02 23:30:04 +0200

    no message

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 
 const app = express()
 const host = process.env.HOST || '127.0.0.1'
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 app.set('port', port)
 app.use(bodyParser.json());
